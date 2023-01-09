3da26d1680be7938317e3df5842fc8d95df5bf67
jacknimmble6 <79076036+jacknimmble6@users.noreply.github.com>
2023-01-09 18:47:59 -0500

    Add index.html

--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -6,9 +6,9 @@ const ImageSlider = () => {
   .3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80`
 
   return (
-    <div>
+    <div className="xs:w-[1414px] md:w-[1442px]"> 
       <Image loader={() => src} src={src} alt={''} width={1536} style={{ width: '100%'}} 
-      className='h-[550px]' height={10} />
+      className='h-[550px] xs:w-[1440px] md:w-[1520px]' height={10} />
       <p className='absolute w-[400px] top-[210px] left-[50px] text-[80px] text-[#176d73]'>A better way to stay</p>
     </div>
   )
