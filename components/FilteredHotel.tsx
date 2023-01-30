931a6b63beca7529b96de006d75e97825dd8dc21
jacknimmble6 <79076036+jacknimmble6@users.noreply.github.com>
2023-01-30 03:27:02 -0500

    Commit message

--- components/FilteredHotel.tsx
+++ components/FilteredHotel.tsx
@@ -84,11 +84,11 @@ const FilteredHotel = ({ hotelData }: any) => {
                     <>
                       <Image src={x} className="-mt-[145px] z-0 select-none h-[280px] rounded-3xl ml-8" 
                       alt="Picture" width={450} height={280}/>
-                      {imageNumber > 4 ? '' : 
+                      {imageNumber >= 4 ? '' : 
                         <IoChevronForwardCircleOutline className="absolute text-5xl xs:ml-[350px] ml-[430px] -mt-40 text-[#176d73]" 
                         onClick={() => forward(hotel._id)}/>
                       }
-                      {imageNumber < 0 ? '' :
+                      {imageNumber <= 0 ? '' :
                         <IoChevronBackCircleOutline className="absolute -mt-[160px] text-5xl ml-[35px] text-[#176d73]" 
                         onClick={() => backward()}/>
                       }
