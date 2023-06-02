bef2665e55cc26e898a79fd3640b3c4a03533d46
jacknimmble6 <79076036+jacknimmble6@users.noreply.github.com>
2023-06-02 02:36:27 -0400

    my changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,15 @@ const Home = () => {
       <p className='text-[#176d73] text-3xl ml-40 w-[490px] mt-8 xs:w-[700px]'>
         From a room for a night to a loft for as long as you like, there’s a Sonder for every occasion.
       </p>
+
+      <p className='text-[#176d73] text-4xl ml-40 w-[490px] mt-16 xs:w-[700px] underline'>
+        Cities Available
+      </p>
+
+      <ul className='text-[#176d73] text-3xl ml-40 w-[490px] mt-8 xs:w-[700px]'>
+        <li className='mt-4'>Chicago</li>
+        <li className='mt-4'>Pittsburgh</li>
+      </ul>
     </div>
   )
 }
